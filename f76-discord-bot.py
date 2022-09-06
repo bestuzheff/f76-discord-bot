71a744c0b9102df491ea39b796a9f36f2c3e204d
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2022-09-06 21:49:50 +0300

    add new commands

--- f76-discord-bot.py
+++ f76-discord-bot.py
@@ -17,7 +17,9 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    activity = discord.Activity(type=discord.ActivityType.watching, name="!help")
+    await client.change_presence(status=discord.Status.online, activity = activity)
+    print(f"We have logged in as {client.user}")
 
 
 @client.event
@@ -26,18 +28,61 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # Вывод справки
+    desc = f"На сервере у {message.author}"
+
+    # справка
+    if message.content == '!h' or message.content == '!help':
+        emb = discord.Embed(title='СПРАВКА', color=discord.Colour.dark_orange())
+        emb.set_thumbnail(url="https://bestuzheff.github.io/images/f76_nuca_cola.webp")        
+        emb.add_field(name='Команды бота:', value="""
+        **!h** - справка по боту 
+        **!к** - коды запуска ракет 
+        **!м** - событие Горелая земля
+        **!э** - событие Колоссальная проблема
+        **!б** - событие Криптидография\n""", inline=False)
+        emb.set_footer(text='По вопросам работы бота писать @bestuzheff#3788')
+        await message.channel.send(embed=emb)
+
+    # Вывод кодов запуска ракет
     if message.content == '!к' or message.content == '!k':
         # Получим коды запуска ракет
         codes = get_nuka_codes()
         if len(codes) == 3:
-            emb = discord.Embed(title='☢ Коды запуска ракет ☢', color=discord.Colour.dark_orange())
-            emb.add_field(name='Альфа:', value=codes[0], inline=True)
-            emb.add_field(name='Браво:', value=codes[1], inline=True)
-            emb.add_field(name='Чарли:', value=codes[2], inline=True)
-            emb.set_footer(text='По вопросам работы бота писать @bestuzheff#3788')
+            emb = discord.Embed(title='КОДЫ ЗАПУСКА РАКЕТ', color=discord.Colour.dark_orange())
+            emb.set_thumbnail(url="https://bestuzheff.github.io/images/f76_bomb.webp")
+            emb.add_field(name='Альфа', value=codes[0], inline=True)
+            emb.add_field(name='Браво', value=codes[1], inline=True)
+            emb.add_field(name='Чарли', value=codes[2], inline=True)
         else:
             emb = discord.Embed(title="Получить коды запуска ракет не удалось!")
+        await message.channel.send(embed=emb)
+
+    # События
+    # Матка
+    if message.content.startswith('!м'):
+        emb = discord.Embed(title='СОБЫТИЕ ГОРЕЛАЯ ЗЕМЛЯ',description=desc, color=0x7289da)
+        emb.set_thumbnail(url="https://bestuzheff.github.io/images/f76_Scorched_quest.webp")
+        note = get_note(message.content)
+        if note != '':
+            emb.add_field(name='Примечание', value=note, inline=False)
+        await message.channel.send(embed=emb)
+
+    # Эрл
+    if message.content.startswith('!э'):
+        emb = discord.Embed(title='СОБЫТИЕ КОЛОССАЛЬНАЯ ПРОБЛЕМА',description=desc, color=0x7289da)
+        emb.set_thumbnail(url="https://bestuzheff.github.io/images/f76_wst_colossus.webp")
+        note = get_note(message.content)
+        if note != '':
+            emb.add_field(name='Примечание', value=note, inline=False)
+        await message.channel.send(embed=emb)
+
+    # Баран
+    if message.content.startswith('!б'):
+        emb = discord.Embed(title='СОБЫТИЕ КРИПТИДОГРАФИЯ',description=desc, color=0x7289da)
+        emb.set_thumbnail(url="https://bestuzheff.github.io/images/f76_cryptide.png")
+        note = get_note(message.content)
+        if note != '':
+            emb.add_field(name='Примечание', value=note, inline=False)
         await message.channel.send(embed=emb)
 
 
@@ -54,5 +99,8 @@ def get_nuka_codes():
                 codes.append(nuca_сode_text)
     return codes
     
+def get_note(message):
+    note = message[2:].strip()
+    return note
 
 client.run(bot_token)
